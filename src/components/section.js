26e86e86d5b5601673fcc966b2139e1d77afc629
Diego <diegolmi@hotmail.com>
2020-04-10 03:51:41 -0300

    Saque carousel

--- src/components/section.js
+++ src/components/section.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+// import Carousel from 'react-bootstrap/Carousel';
 import Perro1 from './images/dogger_img_sm_3.jpg';
-import Perro2 from './images/dogger_img_sm_1.jpg';
-import Perro3 from './images/dogger_img_sm_2.jpg';
+// import Perro2 from './images/dogger_img_sm_1.jpg';
+// import Perro3 from './images/dogger_img_sm_2.jpg';
 import './style.css';
 
 class Section extends React.Component {
@@ -20,43 +20,18 @@ class Section extends React.Component {
                     </div>
 
                 </div>
-                <Carousel>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={Perro1}
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>Doctores</h3>
-                            <p>Encontraras los mejores profesionales para cuidar a tu mascota</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={Perro2}     
-                           alt="Third slide"
-                        />
+                <div className="row hover-1-wrap mb-5 mb-lg-0">
 
-                        <Carousel.Caption>
-                            <h3>Peluqueria</h3>
-                            <p>Ofertas unicas en nuestro salon de belleza</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={Perro3}
-                             alt="Third slide"
-                        />
+                    <div className="mb-4 mb-lg-0 col-lg-6 order-lg-2" data-aos="fade-right">
 
-                        <Carousel.Caption>
-                        <h3>Peluqueria</h3>
-                            <p>Ofertas unicas en nuestro salon de belleza</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+                        <img src={Perro1} alt="" />
+                    </div>
+                    <div className="col-lg-5 mr-auto text-lg-right align-self-center order-lg-1" data-aos="fade-left">
+                        <h2 className="text-black">Veterinaria</h2>
+                        <p className="mb-4">Tenemos a los mejores profesionales para el cuidado de tu mascota.</p>
+                        <p></p>
+                    </div>
+                </div>
             </>
 
 
