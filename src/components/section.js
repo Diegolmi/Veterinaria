647033e3bd7429836fa00f84b9902353db57f61b
Diego <diegolmi@hotmail.com>
2020-04-11 01:46:49 -0300

    Centrado

--- src/components/section.js
+++ src/components/section.js
@@ -18,30 +18,30 @@ class Section extends React.Component {
                     </div>
 
                 </div>
-                <div className="row hover-1-wrap mb-5 mb-lg-0">
+                <div className="row mb-5 mb-lg-0">
 
-                    <div className="mb-4 mb-lg-0 col-lg-6 order-lg-2" data-aos="fade-right">
-                        <a href="index.html" className="hover-1">
+                    <div className="mb-4 mb-lg-0 col-lg-6 order-lg-2" >
+                        <a href="index.html" className="">
                             <img src={Perro1} alt="" class="img-fluid" />
                         </a>
                     </div>
-                    <div className="col-lg-5 mr-auto text-lg-right align-self-center order-lg-1" data-aos="fade-left">
+                    <div className="col-md-7 align-self-center text-center text-md-left" >
                         <h2 className="text-black">Veterinaria</h2>
-                        <p className="mb-4">Tenemos a los mejores profesionales para el cuidado de tu mascota.</p>
-                        <p></p>
+                        <p className="mb-4">Estan los mejores profesionales para el cuidado de tu mascota.</p>
+                        <p><a href="turnos.html" class="btn btn-primary">Reservar Turno</a></p>
                     </div>
                 </div>
                 <div className="row hover-1-wrap mb-5 mb-lg-0">
                     <div className="col-12">
                         <div className="row">
-                            <div className="mb-4 mb-lg-0 col-lg-6" data-aos="fade-left">
+                            <div className="mb-4 mb-lg-0 col-lg-6">
                                 <a href="tienda.html" className="hover-1">
                                     <img src={Juguetes} alt="" className="img-fluid" />
                                  </a>
                             </div>
-                                <div className="col-lg-5 ml-auto align-self-center" data-aos="fade-right">
+                                <div className="col-md-7 align-self-center text-center text-md-left">
                                     <h2 className="text-black">Tienda Online</h2>
-                                    <p className="mb-4">Porque la salud mental de tu mascota tambien importa</p>
+                                    <p className="">Porque la salud mental de tu mascota tambien importa</p>
                                     <p><a href="tienda.html" className="btn btn-primary">ir a la tienda</a></p>
                                 </div>
                             </div>
